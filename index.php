0aed808e09d34b0267a31300c11def8d142b2669
vuthy32 <vuthy32@gmail.com>
2014-01-21 17:04:38 +0700

    CHange Project Name6df WOW I FOUND IT

--- index.php
+++ index.php
@@ -11,9 +11,9 @@ and open the template in the editor.
         <script>
             alert("I add top");
             alert("Hello gitHub!");
-            alert('Kean IT Khmer');
-            alert("I also add bottom");xxxxx
+          Hi VUTHY NEW CODE 
         </script>
+        ABC DEFGCH
         <style>
             img{width:100px;height:100px;}
         </style>
@@ -21,7 +21,7 @@ and open the template in the editor.
     
     <body>
         <?php
-            echo "the second!";
+            TEST FROM OTHER
             echo "this is the first time we use GitHub";
             // put your code here
               echo "the second!";
@@ -29,5 +29,6 @@ and open the template in the editor.
         <img src="banner.jpg"/>
         <input type="button" id="btn1" value="click me"/>
         <img src="banner.jpg"/>
+        <DIV>HI KLOUND</div>
     </body>
 </html>
