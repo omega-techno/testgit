42eeb66530eef24403721d001973fa3f28a36644
vuthy32 <vuthy32@gmail.com>
2014-01-21 16:53:42 +0700

    CHange Project Name5

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ and open the template in the editor.
         <title></title>
         <script>
             alert("Hello gitHub!");
+            alert('Kean IT Khmer');
         </script>
     </head>
     
